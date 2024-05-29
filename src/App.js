58df1429b8e739da90574a6cc2a741dfd18df719
DanBGF <oanceadan111@gmail.com>
2024-05-29 18:28:29 +0300

    bugged

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {Home} from './Pages';
+import logo from './img/shared/logo.svg';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <div>
+ <nav className='navbar  navbar-expand-sm mt-5'>
+      <div className='container-fluid'>
+        <img className='navbar-brand' alt='logo' src = {logo}/>
+
+      </div>
+
+      <div class="navbar-collapse" id="navbarNav">
+        <hr/>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <button class="nav-link active" aria-current="page"><span>01</span>Home</button>
+          </li>
+          <li class="nav-item">
+            <button class="nav-link"><span>02</span>Destination</button>
+          </li>
+          <li class="nav-item">
+            <button class="nav-link"><span>03</span>Crew</button>
+          </li>
+        </ul>
+      </div>
+
+
+    </nav>
+    <Home/> 
+ </div>
+   
   );
 }
 
